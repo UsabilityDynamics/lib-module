18be66629bed15e3464eb30b87c9f1230d25392c
Maxim Peshkov <maxim.peshkov@usabilitydynamics.com>
2014-06-23 20:51:24 +0300

    n/a

--- test/config/wp-config.php
+++ test/config/wp-config.php
@@ -1,7 +1,7 @@
 <?php
 
 /**  */
-define( 'ABSPATH', TEST_ROOT_PATH . '/vendor/usabilitydynamics/lib-wp-phpunit/wordpress/' );
+define( 'ABSPATH', dirname( dirname( dirname( __FILE__ ) ) ) . '/vendor/usabilitydynamics/lib-wp-phpunit/wordpress/' );
 
 /** */
 define( 'DB_NAME', 'circle_test' );
